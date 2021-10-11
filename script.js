6f0c3ed07fd562764a34acffe58d9fa3911ef8f1
MelanyKaren <melany10vg@gmail.com>
2021-10-11 12:16:35 -0500

    comentario

---
+++ script.js
@@ -0,0 +1,15 @@
+let canción = document.getElementById("canción");
+let HIHAT_btn = document.getElementById("HIHAT-btn");
+let KICK_btn = document.getElementById("KICK-btn");
+let OPENHAT_btn = document.getElementById("OPENHAT-btn");
+let BOOM_btn = document.getElementById("BOOM-btn");
+let RIDE_btn = document.getElementById("RIDE-btn");
+let SNARE_btn = document.getElementById("SNARE-btn");
+let TOM_btn = document.getElementById("TOM-btn");
+let TINK_btn = document.getElementById("TINK-btn");
+play_btn.addEventListener("click", ()=>{
+    canción.play();
+});
+pause_btn.addEventListener("click", ()=>{
+    canción.play();
+});
